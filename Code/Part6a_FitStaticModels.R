df0b3228e1ea94051da82fe5ca0595fa1906fd87
Becker <BenBecker@nps.gov>
2022-07-26 16:11:20 -0700

    updated fit stats

--- Code/Part6a_FitStaticModels.R
+++ Code/Part6a_FitStaticModels.R
@@ -250,7 +250,7 @@ mod04b_stacked_detformulas <- c('~1','~1+LateEffect','~1+LateEffect') # site OR 
 ### Fit Model 
 mod04b_stacked_fit <- occuMS(detformulas=mod04b_stacked_detformulas, 
                              psiformulas=mod04b_stacked_psiformulas,
-                             data=umf_stacked, parameterization = "condbinom")
+                             data=umf_stacked, parameterization = "multinom") # changed to multinom for GOF tests below
 mod04b_stacked_fit
 #coef(mod04b_stacked_fit)
 
@@ -260,6 +260,60 @@ mod04b_stacked_psi_predict = predict(mod04b_stacked_fit, type = "psi") # est. fo
 mod04b_stacked_det_predict = predict(mod04b_stacked_fit,type='det') # est. for p1, p2, delta
 #lapply(mod04b_stacked_phi_predict,head)
 
+
+
+### Goodness of fit
+
+# AICcmodavg::mb.gof.test(mod04b_stacked_fit) # doesn't work with occuMS()
+# residuals
+plot(mod04b_stacked_fit) #nothing too crazy
+
+
+## trying patch from Ken Kellner to run ranef and parpoot on model with NAs (2022-07-26)
+source("Code/occuMS_patch.R") 
+ranef(mod04b_stacked_fit)  # Works !
+parboot(mod04b_stacked_fit) # works !
+
+# Stats for parboot to fit
+# basically want chi.sq and c-hat
+# function
+fitstats <- function(mod04b_stacked_fit, 
+                     method = "nonparboot") {
+  observed <- getY(mod04b_stacked_fit@data)
+  expected <- fitted(mod04b_stacked_fit)
+  resids <- residuals(mod04b_stacked_fit,
+                      method = "nonparboot")
+  sse <- sum(resids^2,
+             na.rm = TRUE)
+  chisq <- sum((observed - expected)^2 / expected,
+               na.rm = TRUE)
+  freeTuke <- sum((sqrt(observed) - sqrt(expected))^2, 
+                  na.rm = TRUE)
+  out <- c(SSE = sse,
+           Chisq = chisq,
+           freemanTukey = freeTuke)
+  return(out)
+}
+
+#run parboot
+pb <- parboot(mod04b_stacked_fit,
+              fitstats,
+              nsim = 100,
+              report = TRUE,
+              method = "nonparboot")
+pb
+
+# plot space
+par(mfrow = c(3,1))
+
+plot(pb,
+     main = "",
+     xlab = c("SSE", "Chisq", "FT"),
+     ask = FALSE)
+
+#c-hat equation from google group
+cHat_pb <- pb@t0 / mean(pb@t.star)
+cHat_pb
 
 
 
