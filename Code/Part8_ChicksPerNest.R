f522ec70699a5ebf53082b7aec36d9c78897d8c4
Becker <benbecker@nps.gov>
2023-02-26 16:13:51 -0800

    update

--- Code/Part8_ChicksPerNest.R
+++ Code/Part8_ChicksPerNest.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(tidyr)
 
 ChicksPerNest <- read_csv('Data/ChickPerNest.csv') 
-View(ChicksPerNest)
+#View(ChicksPerNest)
 
 unique (ChicksPerNest$Variable)
 ChicksPerNest <- ChicksPerNest %>% filter(Variable != "PEFAChicksPerNest")
@@ -15,27 +15,30 @@ p.fledge <- ggplot(ChicksPerNest, aes(Year, Value,
                                       linetype = Variable,
                                       shape = Variable)) +
   geom_pointrange(aes(ymin=Value-SD, ymax = Value+SD), position = position_dodge(width = 0.2), size = 0.75) +
-  geom_line(aes(linetype=Variable), size = 1, position = position_dodge(width = 0.2)) +
+  geom_line(aes(linetype=Variable), size = .75, position = position_dodge(width = 0.2)) +
   theme_classic(base_size = 20) +
   ylab(NULL) +
   theme(legend.position = "none") +
   geom_text(x=2016, y=1.75, label="Nesting PEFA Pairs", 
             #color = "#F8766D", 
-            size = 6) +
+            size = 5) +
   geom_text(x=2017, y=5, label="Mean(SD) PRFA \nfledges per nest", 
             #color = "#00BFC4", 
-            size = 6) +
+            size = 5) +
   scale_y_continuous(limits = c(0, 5.5), breaks = c(seq(0,5,by = 1))) + 
   scale_x_continuous(limits = c(2006.5, 2021.1), breaks = c(seq(2008,2020,by = 2))) 
   
 p.fledge
+
+
+ggsave("Output/Fig4.png", width = 8, height = 6, units = "in", dpi = 300)
 
 ## r2
 
 t1 <- ChicksPerNest %>% select(-SD) %>%
   pivot_wider(names_from = Variable, values_from = Value) 
    
-with(t1, cor.test(NestingPEFAPairs, PRFAChicksPerNest))
+with(t1, cor.test(NestingPEFAPairs, PRFAChicksPerNest, method = "spearman"))
 
 with(t1, plot(NestingPEFAPairs, PRFAChicksPerNest))
 
